92ca713129e3b503b2560e652ee9bb98d0e5a9c5
Sunath20 <SunathThenujaya48@gmail.com>
2026-03-22 19:15:35 +0530

    deploy

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("C:\\Users\\lol44\\Desktop\\next-level-backend-docs\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\lol44\\Desktop\\next-level-backend-docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\lol44\\Desktop\\next-level-backend-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\lol44\\Desktop\\next-level-backend-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\lol44\\Desktop\\next-level-backend-docs\\src\\css\\custom.css"),
+];
